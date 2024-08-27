c0c404696fb427034033d2fc39a7a9deb9bd01df
Y Nguyen Minh <YNM2@fpt.com.vn>
2024-08-28 01:17:08 +0700

    edit

--- Controllers/AltaTestController.cs
+++ Controllers/AltaTestController.cs
@@ -25,13 +25,13 @@ namespace AltaTest.Controllers
         {
             IEnumerable<View01> view01s = new List<View01>();
             StringBuilder query = new StringBuilder();
-            query.Append("ROW_NUMBER() OVER (ORDER BY sum(b.point) DESC, max(b.DateTime), a.Name, b.PlayerId) AS STT, ");
+            query.Append("select ROW_NUMBER() OVER (ORDER BY sum(b.point) DESC, max(b.DateTime), a.Name, b.PlayerId) AS STT, ");
             query.Append("    b.PlayerId, a.Name, a.Gender, sum(b.point) as point, max(b.DateTime) as DateTime ");
             query.Append("from Players a ");
             query.Append("    left join Points b on a.Id = b.PlayerId ");
             query.Append("group by b.PlayerId, a.Name, a.Gender ");
             query.Append("order by point DESC, DateTime, Name, PlayerId ");
-            view01s = _context.Database.SqlQuery<View01>(query.ToString());
+            view01s = _context.Database.ExecuteSql(query.ToString());
             return view01s;
         }
     }
